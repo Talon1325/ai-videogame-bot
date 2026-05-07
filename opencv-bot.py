7e3ed912fc3ba7162c5dea0c10980a45de109089
Talon1325 <talonvorpahl@gmail.com>
2026-05-07 18:17:44 -0500

    initialize new opencv bot

---
+++ opencv-bot.py
@@ -0,0 +1,8 @@
+# Author: Talon Vorpahl
+# Update my hardcoded bot to shot at targets available instead of shooting at every location in 3x3 grid in aimlabs
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
